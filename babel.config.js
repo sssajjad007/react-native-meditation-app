459e46d74b2c712c9ae4ce6efa6837730f5b30e4
sssajjad007 <sssajjad.76@gmail.com>
2022-05-28 18:42:17 +0430

    add jest packages and config

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: [
+      "babel-preset-expo",
+      "@babel/preset-typescript",
+    ],
+
     plugins: [
       "@babel/plugin-proposal-numeric-separator",
-      "react-native-reanimated/plugin",
     ],
   };
 };
